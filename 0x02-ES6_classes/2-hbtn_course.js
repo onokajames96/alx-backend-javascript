ecfc3eb9918d4806967d84b0a23345c46e9ce6a3
onokajames96 <jamesonoka996@gmail.com>
2024-03-07 11:40:49 +0000

    classes

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,42 @@
+export default class HolbertonCourse {
+    constructor(name, length, students) {
+	//Create objs
+        this.name = name;
+        this.length = length;
+        this.students = students;
+    }
+
+    // Getter and setter
+    get name() {
+        return this._name;
+    }
+    set name(value) {
+        if (typeof value !== 'string') {
+            throw new TypeError('Name must be a string');
+        }
+        this._name = value;
+    }
+
+    // Getter and setter
+    get length() {
+        return this._length;
+    }
+    set length(value) {
+        if (typeof value !== 'number') {
+            throw new TypeError('Length must be a number');
+        }
+        this._length = value;
+    }
+
+    // Getter and setter
+    get students() {
+        return this._students;
+    }
+    set students(value) {
+        if (!Array.isArray(value) || !value.every(item => typeof item === 'string')) {
+            throw new TypeError('Students must be an array of strings');
+        }
+        this._students = value;
+    }
+}
+
