0a5f33d87ad21f297fe069420410fe2550e537e9
onokajames96 <jamesonoka996@gmail.com>
2024-03-12 18:37:28 +0000

    mapping

---
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -0,0 +1,15 @@
+function updateUniqueItems(map) {
+  if (!(map instanceof Map)) {
+    throw new Error('Cannot process');
+  }
+
+  map.forEach((value, key, map) => {
+    if (value === 1) {
+      map.set(key, 100);
+    }
+  });
+
+  return map;
+}
+
+export default updateUniqueItems;
