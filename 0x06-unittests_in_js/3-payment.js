6a4bb1044008317b485f05d3200b1cd6657b88b2
onokajames96 <jamesonoka996@gmail.com>
2024-05-03 13:13:53 +0000

    tests 3

---
+++ 0x06-unittests_in_js/3-payment.js
@@ -0,0 +1,8 @@
+const Utils = require('./utils');
+
+function sendPaymentRequestToApi(totalAmount, totalShipping) {
+  const sumResult = Utils.calculateNumber('SUM', totalAmount, totalShipping);
+  console.log(`The total is: ${sumResult}`);
+}
+
+module.exports = sendPaymentRequestToApi;
